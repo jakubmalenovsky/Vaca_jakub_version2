375ee6722a8100749369776f439e155b083741c1
@jakubmalenovsky <eaa24jmal@students.eaaa.dk>
2024-09-23 12:54:20 +0200

    Version3

--- script.js
+++ script.js
@@ -1,17 +1,49 @@
+let langs = document.getElementById("language-big-selector"),
+            link = document.querySelectorAll("#language-big-selector a"),
+            title = document.querySelector("h1"),
+            descr = document.querySelectorAll("#where-eat-card h2");
 
+        let data = {
+            en: {
+                title: "Where would you like to eat?",
+                descriptions: ["Dine in", "Take out"]
+            },
+            dk: {
+                title: "Hvor spiser du?",
+                descriptions: ["Spise her", "Takeaway"]
+            }
+        };
+
+        link.forEach(el => {
+            el.addEventListener("click", (event) => {
+                event.preventDefault();
+                langs.querySelector(".active")?.classList.remove("active");
+                el.classList.add("active");
+
+                let attr = el.getAttribute("language");
+                title.textContent = data[attr].title;
+
+                descr.forEach((h2, index) => {
+                    h2.textContent = data[attr].descriptions[index];
+                });
+            });
+        });
+
+
+/*
 let langs = document.getElementById("language-big-selector"),
     link = document.querySelectorAll("#language-big-selector a"),
     title = document.querySelector("h1"),
     descr = document.querySelectorAll("#where-eat-card h2");
-
+    
 let data = {
     en: {
         title: "Where would you like to eat?",
         descriptions: ["Dine in", "Take out"] 
     },
     dk: {
-        title: "Hvor vil du gerne spise?",
-        descriptions: ["Spis inde", "Tag med"] 
+        title: "Hvor spiser du?",
+        descriptions: ["Spise her", "Takeaway"] 
     }
 };
 
@@ -23,6 +55,7 @@ link.forEach(el => {
 
         let attr = el.getAttribute("language"); 
         title.textContent = data[attr].title;
+
         
        
         descr.forEach((h2, index) => {
@@ -30,3 +63,4 @@ link.forEach(el => {
         });
     });
 });
+*/
